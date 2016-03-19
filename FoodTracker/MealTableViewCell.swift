a4a42e0bf526b955c56d6a5d38e8a4d3209ee783
Siyi Tu <st638@cornell.edu>
2016-03-19 10:38:51 -0400

    Added table view and basic navigation

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Cynthia Tu on 3/18/16.
+//  Copyright © 2016 Cynthia Tu. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    // MARK: Properties
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
